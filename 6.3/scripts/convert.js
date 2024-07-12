086a027570eba023f01409e9f052bf56d0a500d1
Shaobin-Jiang <shaobin-jiang@outlook.com>
2024-07-12 18:00:50 +0800

    Removed everything with mike 2.1.2

--- 6.3/scripts/convert.js
+++
@@ -1,9 +0,0 @@
-const origin = document.location.origin;
-for (let aElem of document.querySelectorAll("a")) {
-    if (aElem.href == origin + "/overview/plugins.html#parameters-available-in-all-plugins") {
-        aElem.href = origin + "/overview/plugins.html#_3";
-    }
-    if (aElem.href == origin + "/overview/plugins.html#data-collected-by-all-plugins") {
-        aElem.href = origin + "/overview/plugins.html#_4";
-    }
-}
